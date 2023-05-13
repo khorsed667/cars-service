153060f85bc24c2b897a6c1bbb5f170305ff05b6
khorsed667 <khorsedalam667@gmail.com>
2023-05-13 21:07:10 +0600

    login and signup added

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -3,21 +3,22 @@ import loginImg from './../../assets/images/login/login.svg'
 import { useContext } from 'react';
 import { AuthContext } from '../../Provider/AuthProvider';
 
-const Login = () => {
+const SignUp = () => {
 
-    const {signIn} = useContext(AuthContext)
+    const {createUser} = useContext(AuthContext)
 
-    const handelLogin = event => {
+    const handelsignup = event => {
         event.preventDefault();
         const form = event.target;
+        const name = form.name.value;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(email, password);
+        console.log(name, email, password);
 
-        signIn(email, password)
-        .then(res =>{
-            const signedUser = res.user
-            console.log(signedUser);
+        createUser(email, password)
+        .then(result =>{
+            const loggedUser = result.user
+            console.log(loggedUser);
         })
         .catch(error => console.log(error))
     }
@@ -30,33 +31,39 @@ const Login = () => {
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
-                        <h1 className="text-3xl text-orange-500 font-bold">Login now!</h1>
-                        <form onSubmit={handelLogin}>
+                        <h1 className="text-3xl text-orange-500 font-bold">SignUp now!</h1>
+                        <form onSubmit={handelsignup}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" autoComplete="off" name='name' placeholder="Name" className="input input-bordered" />
+                            </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                                <input type="text" name='email' placeholder="Email" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                                <input className='btn btn-warning text-white' type="submit" value="Login" />
+                                <input className='btn btn-warning text-white' type="submit" value="Sign Up" />
                             </div>
                         </form>
-                        <p className='py-2 text-center'>New to CarsDoctor? <Link className='text-orange-500 font-bold' to='/signup'>Sign Up</Link> </p>
                     </div>
+                    <p className='my-2 text-center'>Already have an account? <Link className='text-orange-500 font-bold' to='/login'>Login</Link> </p>
                 </div>
             </div>
         </div>
     );
 };
 
-export default Login;
+export default SignUp;
